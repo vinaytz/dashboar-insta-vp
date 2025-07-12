c3accf703a33e2c9ccdc971b994c2813898a87ca
Vinay Tiwari <109553738+vinaytz@users.noreply.github.com>
2025-07-12 19:14:14 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', 
+  base: '/dashboar-insta-vp/', 
   plugins: [react()],
   resolve: {
     alias: {
